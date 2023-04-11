f32415719d07a01c510660f5fac4714fa57ddc62
ANDREIEV VIACHESLAV <andreiev.viacheslav@gmail.com>
2023-04-11 09:48:41 +0300

    ✅ add pagination

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -10,15 +10,22 @@ import { usePosts } from "../hooks/usePosts";
 import PostService from "../API/PostService";
 import { Loader } from "../Components/UI/Loader/Loader";
 import { useFetching } from "../hooks/useFetching";
+import { getPageCount } from "../utils/page";
+import { Pagination } from "../Components/UI/Pagination";
 
 export const App = () => {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
 
   const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll();
-    setPosts(posts);
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers["x-total-count"];
+    setTotalPages(getPageCount(totalCount, limit));
   });
 
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
@@ -34,7 +41,11 @@ export const App = () => {
 
   useEffect(() => {
     fetchPosts();
-  }, []);
+  }, [page]);
+
+  const changePage = (page) => {
+    setPage(page);
+  };
 
   return (
     <div className={styles.App}>
@@ -67,7 +78,11 @@ export const App = () => {
         ) : (
           <PostsList remove={removePost} posts={sortedAndSearchedPosts} />
         )}
-
+        <Pagination
+          page={page}
+          changePage={changePage}
+          totalPages={totalPages}
+        />
         <Modal visible={modal} setVisible={setModal}>
           <PostForm create={createPost} />
         </Modal>
